46fc2c47b5ccda55dd6d600edca940cf2181c0ce
= <=>
2022-11-03 10:34:39 +0700

    push to github

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express'); 
+const app = express() ; 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv/config');
+
+// middlewares 
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// import routes 
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute); 
+
+// Routes
+app.get('/', (req, res) => { 
+    res.send('hello home');
+})
+
+// connect to DB
+const username = encodeURIComponent(process.env.DB_USER);
+const password = encodeURIComponent(process.env.DB_PASSWORD);
+const cluster = encodeURIComponent(process.env.Cluster); 
+const DB_URL = `mongodb+srv://${username}:${password}@${cluster}.adsjksx.mongodb.net/?retryWrites=true&w=majority`; 
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: encodeURIComponent(process.env.DB_NAME)
+      }, () => {
+        console.log('connected');
+      })
+
+
+  } catch (e) {
+    console.log("could not connect");
+  }
+
+// How to use start listening to the server 
+app.listen(process.env.PORT);
---
+++ routes/posts.js
@@ -0,0 +1,73 @@
+const express = require('express'); 
+const router = express.Router() ; 
+const Post = require('../models/Post'); 
+
+// GET back all the posts 
+router.get('/', async (req, res) => { 
+    try {
+        const listPosts = await Post.find();
+        res.json(listPosts);
+    } catch (error) {
+        res.json('loi')
+    }
+})
+
+// submits a post
+router.post('/', async (req,res) => { 
+    const post = new Post({ 
+        title: req.body.title, 
+        description: req.body.description
+    });
+
+    console.log(post);
+
+    try {
+        const savedPost = await post.save(); 
+        res.json(savedPost)
+    } catch (err) {
+        res.json('loi')
+    }
+})
+
+// specific post 
+router.get('/:postId', async (req, res) => { 
+    console.log(req.params.postId);
+    try {
+        const post = await Post.findById(req.params.postId); 
+        res.json(post);
+    } catch (error) {
+        res.json('could not found')
+    }
+})
+
+// delete post 
+router.delete('/:postId', async (req, res) => { 
+    try {
+        const removedPost = Post.deleteOne({_id: req.params.postId}, (err, result) => { 
+            if(err) {
+                console.log(err);
+            } else { 
+                console.log("result : ", result);
+            }
+        }); 
+        res.json(`da xoa thanh cong ${removedPost}`)
+    } catch (error) {
+        res.json('could not delete')
+    }
+});
+
+// update a post 
+router.patch('/:postId' , (req, res) => {
+    console.log(req.body.title);
+    try {
+        const updatePost = Post.where({_id: req.params.postId}).update(
+            {title : req.body.title}
+        )
+        res.json(updatePost)
+    } catch (error) {
+        res.json({message : error})
+    }
+})
+
+
+module.exports = router ; 
